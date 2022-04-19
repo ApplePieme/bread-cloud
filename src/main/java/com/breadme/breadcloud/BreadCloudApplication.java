1a29c66ec9914c88aeff71e11fa215339491b2b1
breadme <breadme@foxmail.com>
2022-04-19 20:18:03 +0800

    初始化项目

---
+++ src/main/java/com/breadme/breadcloud/BreadCloudApplication.java
@@ -0,0 +1,13 @@
+package com.breadme.breadcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreadCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/breadme/breadcloud/BreadCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadme.breadcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
